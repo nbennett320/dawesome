a9035172fd72080c0e1c62fd9a6569e06f08fe53
Sam Kravitz <samkravitz99@gmail.com>
2022-03-31 02:42:24 -0400

    Can pause/unpause sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
 import React from 'react'
-import { useInvoke } from './hooks/swr'
 import './App.scss'
 
+import { invoke } from '@tauri-apps/api/tauri'
 const App = () => {
-  const counter_args = { delta: 1 }
-  const play_args = { delta: 1 }
 
-  const { data: counter, update: increment } = useInvoke(
-    counter_args,
-    'get_counter',
-    'increment_counter'
-  )
-
-  const { data: is_playing, update: toggle_play } = useInvoke(
-    play_args,
-    'get_counter',
-    'toggle_play_sound'
-  )
-  
   return (
     <div className="App">
-      times played: {counter}
-      <br />
-      <button onClick={increment}>
-        Try counter
-      </button> 
-      <br />
-      is playing: {is_playing}
-      <br />
-      <button onClick={toggle_play}>
+      <button onClick={() => invoke('toggle_play')}>
         Play
       </button> 
     </div>
