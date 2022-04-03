a5342cf244ba234973adcf500ff38651dda50513
Tony <tony@karmalies.studio>
2022-04-03 16:44:57 -0500

    Feature/vite + DevOps improvements (#4)
    * Add Vite
    
    * migrate to Vite
    
    * rm obsolete CRA config
    
    * prettier, jest, eslint, and friends
    
    * improve Vite support
    
    * better match Tailwind styling
    
    * Enable React 18

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'airbnb', 'airbnb-typescript', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['prettier', '@typescript-eslint', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    'jest.config.ts',
+    'jest.setup.ts',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'postcss.config.js',
+    'tailwind.config.js',
+    'backend/**',
+    'pnpm-lock.yaml',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: [__dirname],
+      },
+    ],
+    '@typescript-eslint/naming-convention': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,29 @@
-import React from 'react'
-import { useInvoke } from './hooks/swr'
+import React from 'react';
+import { useInvoke } from './hooks/swr';
 
-const App = () => {
-  const counter_args = { delta: 1 }
-  const play_args = { delta: 1 }
+function App() {
+  const counter_args = { delta: 1 };
+  const play_args = { delta: 1 };
 
-  const { data: counter, update: increment } = useInvoke(
-    counter_args,
-    'get_counter',
-    'increment_counter'
-  )
+  const { data: counter, update: increment } = useInvoke(counter_args, 'get_counter', 'increment_counter');
 
-  const { data: is_playing, update: toggle_play } = useInvoke(
-    play_args,
-    'get_counter',
-    'toggle_play_sound'
-  )
+  const { data: is_playing, update: toggle_play } = useInvoke(play_args, 'get_counter', 'toggle_play_sound');
 
   return (
-    <div className="grid place-content-center p-3">
+    <div className="grid p-3 place-content-center">
       times played: {counter}
       <br />
-      <button className="bg-indigo-700 text-white p-3 rounded-sm" onClick={increment}>
+      <button type="button" className="p-3 text-white bg-indigo-700 rounded-sm" onClick={increment}>
         Try counter
       </button>
       <br />
       is playing: {is_playing}
       <br />
-      <button className="bg-violet-700 text-white p-3 rounded-sm" onClick={toggle_play}>
+      <button type="button" className="p-3 text-white rounded-sm bg-violet-700" onClick={toggle_play}>
         Play
       </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
