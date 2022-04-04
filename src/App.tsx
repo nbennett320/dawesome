a0c9cd5bdb44b10d912cd07791b09f2e9c787298
Noah Bennett <nbennett@athn.org>
2022-04-04 15:01:44 -0400

    changing tempo with input field

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useInvoke } from './hooks/swr'
 import './App.scss'
 
 const App = () => {
+  // const [tempo, setTempo] = React.useState(() => invoke('get_playlist_tempo').then(x => x))
+  const [tempo, setTempo] = React.useState(120.)
   const [runtime, setRuntime] = React.useState<number>()
   const timer = React.useRef<any>(null)
   const { data: playing, update: togglePlay } = useInvoke(
@@ -11,6 +13,12 @@ const App = () => {
     'get_playlist_playing',
     'toggle_playlist'
   )
+
+  // const { data: tempo, update: setTempo } = useInvoke(
+  //   { val: 120 },
+  //   'get_playlist_tempo',
+  //   'set_playlist_tempo'
+  // )
   
   React.useEffect(() => {
     if(playing) {
@@ -25,11 +33,20 @@ const App = () => {
     }
   }, [playing])
 
+  React.useEffect(() => {
+    invoke('set_playlist_tempo', { val: tempo as any })
+  }, [tempo])
+
   return (
     <div className="App">
       <button onClick={togglePlay}>
         {playing ? <>Pause</> : <>Play</> }
       </button> 
+
+      <label>Tempo:</label>
+      <input type='number' value={tempo as any} onInput={(e) => { setTempo(parseFloat(e.currentTarget.value) as number) }}>
+      </input>
+
       {playing && <div style={{ display: 'flex', flexDirection: 'column', width: '200px', marginLeft: 'auto', marginRight: 'auto'}}>
         <span>Playlist runtime:</span> <span style={{ display: 'flex',flexDirection: 'row' }}><span style={{ width: '100px',left: '0' }}>{runtime}</span> <span style={{ left: '100%' }}>ms</span></span>
       </div>}
