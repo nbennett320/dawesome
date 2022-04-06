fad3c70df1d02bb1d6e25a4c578525e63d12757f
tonyketcham <tony@karmalies.studio>
2022-04-05 19:20:56 -0500

    init redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.scss'
+import { Provider } from 'react-redux'
 import App from './App'
+import { store } from './state/store'
 import reportWebVitals from './reportWebVitals'
 
 const container = document.getElementById('root') as Element
@@ -9,7 +11,9 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
 
