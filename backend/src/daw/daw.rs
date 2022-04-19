d52325655ad41d066af38f7dc543c65f82969c05
Noah Bennett <nbennett.320@gmail.com>
2022-04-19 01:48:27 -0400

    pulse audio output working

--- backend/src/daw/daw.rs
+++ backend/src/daw/daw.rs
@@ -1,3 +1,5 @@
+use psimple;
+use pulse;
 use rodio::Source;
 use rodio::{Decoder, OutputStream, Sink};
 use std::fs::File;
@@ -7,28 +9,26 @@ use std::sync::atomic;
 use std::thread;
 use std::time;
 use tauri;
-use pulse;
-use psimple;
 
 use crate::daw::{daw_core, state};
 
-/** 
-  Play a single sound sample for its entire length
-  - Compiled only on Linux systems,
-  for use with Alsa, Jack, and PulseAudio drivers
-  */ 
+/**
+Play a single sound sample for its entire length
+- Compiled only on Linux systems,
+for use with Alsa, Jack, and PulseAudio drivers
+*/
 #[cfg(target_os = "linux")]
 pub async fn play_sample(path: &str) {
   let path = String::from(path);
   thread::spawn(move || {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let file = BufReader::new(File::open(path).unwrap());
     let source = Decoder::new(file).unwrap();
 
     let spec = pulse::sample::Spec {
-      format: pulse::sample::Format::S24NE,
+      format: pulse::sample::Format::S16NE,
       channels: 2,
-      rate: 44_100
+      rate: 44_100,
     };
 
     let sink = psimple::Simple::new(
@@ -39,24 +39,27 @@ pub async fn play_sample(path: &str) {
       "dawesome output",
       &spec,
       None,
-      None
-    ).unwrap();
+      None,
+    )
+    .unwrap();
 
-    let raw_source: rodio::source::SamplesConverter<_, u16> = source.convert_samples();
-    let raw_vec: std::vec::Vec<u16> = raw_source.collect();
+    let raw_source: rodio::source::SamplesConverter<_, i16> =
+      source.convert_samples();
+    let raw_vec: std::vec::Vec<i16> = raw_source.collect();
 
     unsafe {
       let raw_slice = raw_vec.align_to::<u8>().1;
-      sink.write(raw_slice);
+      sink.write(raw_slice).unwrap();
+      sink.drain().unwrap();
     }
   });
 }
 
-/** 
-  Play a single sound sample for its entire length
-  - Compiled only on Windows and MacOS systems,
-  for use with ALSA and CoreAudio
-  */ 
+/**
+Play a single sound sample for its entire length
+- Compiled only on Windows and MacOS systems,
+for use with ALSA and CoreAudio
+*/
 #[cfg(not(target_os = "linux"))]
 pub async fn play_sample(path: &str) {
   let path = String::from(path);
@@ -78,14 +81,10 @@ pub fn play_metronome(state_ref: &sync::Arc<state::InnerState>) {
 
   if state.playlist_current_beat.load(atomic::Ordering::SeqCst) > 0 {
     // default metronome tick
-    futures::executor::block_on(play_sample(
-      "assets/assets_66-hh-01-or.wav",
-    ));
+    futures::executor::block_on(play_sample("assets/assets_66-hh-01-or.wav"));
   } else {
     // play accented metronome tick
-    futures::executor::block_on(play_sample(
-      "assets/assets_66-hh-01-or-2.wav",
-    ));
+    futures::executor::block_on(play_sample("assets/assets_66-hh-01-or-2.wav"));
   }
 }
 
@@ -97,27 +96,35 @@ pub fn run_playlist(state_ref: &sync::Arc<state::InnerState>) {
   let tempo_intrv_ms = daw_core::timing::tempo_to_intrv_ms(tempo);
 
   pool.exec(move || {
-    thread::spawn(move || {
-      loop {
-        println!("tick");
+    thread::spawn(move || loop {
+      println!("tick");
 
-        if state.metronome_enabled.load(atomic::Ordering::SeqCst) {
-          play_metronome(&state);
-        }
+      if state.metronome_enabled.load(atomic::Ordering::SeqCst) {
+        play_metronome(&state);
+      }
 
-        thread::sleep(time::Duration::from_millis(tempo_intrv_ms));
-        
-        let current_time_signature = state.playlist_time_signature.lock().unwrap();
-        let current_beat = state.playlist_current_beat.load(atomic::Ordering::SeqCst);
-        let next_beat = (current_beat + 1) % current_time_signature.numerator;
-        state.playlist_current_beat.store(next_beat, atomic::Ordering::SeqCst);
+      thread::sleep(time::Duration::from_millis(tempo_intrv_ms));
 
-        state.playlist_total_beats.fetch_add(1, atomic::Ordering::SeqCst);
-        println!("current beat: {}, total beats played: {}",state.playlist_current_beat.load(atomic::Ordering::SeqCst), state.playlist_total_beats.load(atomic::Ordering::SeqCst));
-        
-        if !state.playlist_is_playing.load(atomic::Ordering::SeqCst) {
-          break;
-        }
+      let current_time_signature =
+        state.playlist_time_signature.lock().unwrap();
+      let current_beat =
+        state.playlist_current_beat.load(atomic::Ordering::SeqCst);
+      let next_beat = (current_beat + 1) % current_time_signature.numerator;
+      state
+        .playlist_current_beat
+        .store(next_beat, atomic::Ordering::SeqCst);
+
+      state
+        .playlist_total_beats
+        .fetch_add(1, atomic::Ordering::SeqCst);
+      println!(
+        "current beat: {}, total beats played: {}",
+        state.playlist_current_beat.load(atomic::Ordering::SeqCst),
+        state.playlist_total_beats.load(atomic::Ordering::SeqCst)
+      );
+
+      if !state.playlist_is_playing.load(atomic::Ordering::SeqCst) {
+        break;
       }
     });
   });
@@ -132,8 +139,12 @@ pub fn pause_playlist(state: tauri::State<'_, sync::Arc<state::InnerState>>) {
     .store(false, atomic::Ordering::SeqCst);
 
   // reset playlist to 0 beats
-  state.playlist_current_beat.store(0, atomic::Ordering::SeqCst);
-  state.playlist_total_beats.store(0, atomic::Ordering::SeqCst);
+  state
+    .playlist_current_beat
+    .store(0, atomic::Ordering::SeqCst);
+  state
+    .playlist_total_beats
+    .store(0, atomic::Ordering::SeqCst);
 }
 
 pub fn start_playlist(state: tauri::State<'_, sync::Arc<state::InnerState>>) {
