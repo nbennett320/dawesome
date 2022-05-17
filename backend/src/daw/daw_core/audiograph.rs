ba2f90f198bc82c16815a72cd5dcf52fea574941
Noah Bennett <nbennett.320@gmail.com>
2022-05-17 19:52:13 -0400

    refactoring: storing sinks in audio nodes, graph sample caching

--- backend/src/daw/daw_core/audiograph.rs
+++ backend/src/daw/daw_core/audiograph.rs
@@ -6,38 +6,222 @@ use std::vec::Vec;
 use std::fs::File;
 use std::io::BufReader;
 use futures;
-use rodio::{Decoder};
+use rodio::{Decoder, OutputStream, Sink};
+use rodio::queue::{SourcesQueueOutput};
 use svg::node::element;
 use crate::daw;
 use crate::util;
 
-#[derive(Clone, Debug)]
+#[cfg(target_os = "linux")]
+use psimple;
+#[cfg(target_os = "linux")]
+use pulse;
+
+#[cfg(target_os = "linux")]
+#[derive(Clone)]
 pub struct AudioNode {
   pub id: u64,
   sample_path: String,
   start_time: Option<u64>,
   start_offset: u64,
   track_number: u32,
+  raw_source: sync::Arc<sync::Mutex<Vec<i16>>>,
+  sink: sync::Arc<sync::Mutex<Sink>>,
   handle: sync::Arc<sync::Mutex<Option<thread::JoinHandle<()>>>>,
   running: bool,
 }
 
-impl AudioNode {
+// audio node implementation for Linux
+#[cfg(target_os = "linux")]
+impl AudioNode {  
   pub fn new(
     id: u64,
     sample_path: String,
     start_offset: u64,
     track_number: u32,
   ) -> Self {
+    let file_buf = BufReader::new(File::open(&sample_path).unwrap());
+    let source = Decoder::new(file_buf).unwrap();
+
+    let spec = pulse::sample::Spec {
+      format: pulse::sample::Format::S16NE,
+      channels: 2,
+      rate: 44_100,
+    };
+
+
+    let sink = psimple::Simple::new(
+      None,
+      "dawesome",
+      pulse::stream::Direction::Playback,
+      None,
+      "dawesome output",
+      &spec,
+      None,
+      None,
+    ).unwrap();
+
+    let raw_source: rodio::source::SamplesConverter<_, i16> = source.convert_samples();
+    let raw_source: std::vec::Vec<i16> = raw_source.collect();
+
     AudioNode {
       id,
       sample_path,
       start_time: None,
       start_offset,
       track_number,
+      raw_source,
+      sink: sync::Arc::new(sync::Mutex::from(sink)),
       handle: sync::Arc::new(sync::Mutex::from(None)),
       running: false,
     }
+  }
+
+  // play the audio node
+  pub fn play(self) {
+    let handle = thread::spawn(move || {
+      let sink = self.sink.lock().unwrap();
+      let raw_source = self.raw_source.lock().unwrap();
+
+      unsafe {
+        let raw_slice = raw_source.align_to::<u8>().1;
+        sink.write(raw_slice).unwrap();
+        sink.drain().unwrap();
+      }
+    });
+
+    *self.handle.lock().unwrap() = Some(handle);
+  }
+
+  pub fn stop(self) {
+
+  }
+
+  // calculate and set a node's (real) start time in the playlist
+  pub fn set_start_time(&mut self, start_time: u64) {
+    self.start_time = Some(start_time + self.start_offset);
+  }
+
+  // reset node start time 
+  pub fn clear_start_time(&mut self) {
+    self.start_time = None;
+  }
+
+  // attach a thread handle to a node
+  pub fn set_handle(self, handle: thread::JoinHandle<()>) {
+    *self.handle.lock().unwrap() = Some(handle);
+  }
+
+  // get a path of a node's normalized audio waveform
+  pub fn get_waveform(&self) -> (String, String) {
+    let file = BufReader::new(File::open(&self.sample_path).unwrap());
+    let source = Decoder::new(file).unwrap();
+    let mut samples = std::vec::Vec::<i16>::new();
+
+    println!("raw len: {:?}", source.size_hint());
+    for sample in source {
+      samples.push(sample);
+    }
+
+    // get frames and interpolate points
+    let xs: Vec<i32> = (0..samples.len()).into_iter().map(|x| x as i32).collect();
+    let ys: Vec<i32> = samples.iter().map(|y| *y as i32).collect();
+    // let interp = util::math::local_extremes(xs, ys);
+    let interp = Some((xs as Vec<i32>, ys as Vec<i32>));
+    let (x_interps, y_interps) = interp.unwrap();
+    println!("interp lens: {:?}, {:?}", x_interps.len(), y_interps.len());
+
+    // calculate bounding box
+    let (min_x, min_y) = (0, -y_interps.iter().max().unwrap());
+    let (max_x, max_y) = (x_interps.len(), 2 * (*y_interps.iter().max().unwrap() as i32));
+    let svg = element::SVG::new()
+      .set("viewBox", (min_x, min_y, max_x, max_y));
+
+    // initialize data path
+    let mut data = element::path::Data::new().move_to((0, 0));
+    
+    // fill path
+    for idx in 1..y_interps.len() {
+      let p1 = (idx, y_interps[idx]);
+      data = data.line_to(p1)
+    }
+
+    // close the path
+    data = data.close();
+
+    // fetch calculated path value
+    let path = element::Path::new()
+      .set("stroke", "black")
+      .set("stroke-width", "0.05%")
+      .set("fill", "black")
+      .set("d", data);
+
+    let pathd = path.get_inner().get_attributes().get("d").unwrap();
+    let viewbox = svg.get_inner().get_attributes().get("viewBox").unwrap();
+
+    (pathd.to_string(), viewbox.to_string())
+  }
+}
+
+#[derive(Clone)]
+#[cfg(not(target_os = "linux"))]
+pub struct AudioNode {
+  pub id: u64,
+  sample_path: String,
+  start_time: Option<u64>,
+  start_offset: u64,
+  track_number: u32,
+  sink: sync::Arc<sync::Mutex<Sink>>,
+  handle: sync::Arc<sync::Mutex<Option<thread::JoinHandle<()>>>>,
+  running: bool,
+}
+
+// audio node implementation for Mac/Windows
+#[cfg(not(target_os = "linux"))]
+impl AudioNode {  
+  pub fn new(
+    id: u64,
+    sample_path: String,
+    start_offset: u64,
+    track_number: u32,
+  ) -> Self {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    let file_buf = BufReader::new(File::open(&sample_path).unwrap());
+    let source = Decoder::new(file_buf).unwrap();
+
+    sink.append(source);
+    sink.pause();
+
+    AudioNode {
+      id,
+      sample_path,
+      start_time: None,
+      start_offset,
+      track_number,
+      sink: sync::Arc::new(sync::Mutex::from(sink)),
+      handle: sync::Arc::new(sync::Mutex::from(None)),
+      running: false,
+    }
+  }
+
+  // play the audio node
+  pub async fn play(self) {
+    println!("going to play");
+    thread::spawn(move || {
+      let sink = self.sink.lock().unwrap();
+      println!("playing for {}", sink.empty());
+      
+      sink.play();
+      sink.sleep_until_end();
+      println!("should have played");
+    });
+
+    // *self.handle.lock().unwrap() = Some(handle);
+  }
+
+  pub fn stop(self) {
+
   }
 
   // calculate and set a node's (real) start time in the playlist
@@ -183,18 +367,16 @@ impl AudioGraph<'static> {
       let running = std::sync::Arc::new(std::sync::Mutex::new(self_arc.lock().unwrap().running));
 
       // run time slice
-      let handle = thread::spawn(move || {
+      thread::spawn(move || {
         // calculate time until sample is played
         let dur = *start_offset.lock().unwrap() - *current_offset.lock().unwrap();
         
         // sleep this thread until it's time to play the sample, then play it
         thread::sleep(time::Duration::from_millis(dur));
         if *running.lock().unwrap() {
-          futures::executor::block_on(daw::play_sample(&*sample_path.lock().unwrap()));
+          futures::executor::block_on(daw::play_sample(&sample_path.lock().unwrap()));
         }
       });
-
-      node.set_handle(handle);
     }
   }
 
