cf39d5f8409c26e8492a1695b184e55fe3b15368
Noah Bennett <nbennett.320@gmail.com>
2022-05-17 20:10:28 -0400

    adding load/save menu

--- backend/src/app/window.rs
+++ backend/src/app/window.rs
@@ -7,6 +7,7 @@ struct EventPayload {
   data: String,
 }
 
+// initialize the application
 pub fn setup(
   app: &mut tauri::App<>
 ) -> Result<(), Box<dyn std::error::Error>> {
@@ -30,11 +31,19 @@ pub fn setup(
   Ok(())
 }
 
+// build the containing window menus
 pub fn build_menu() -> tauri::Menu {
-  // define file menu
+  // define main menu
   let quit_menu = tauri::CustomMenuItem::new("quit".to_string(), "Quit");
-  let file_submenu = tauri::Submenu::new("File", tauri::Menu::new()
+  let main_submenu = tauri::Submenu::new("Dawesome".to_string(), tauri::Menu::new()
     .add_item(quit_menu));
+
+  // define file menu
+  let save_menu = tauri::CustomMenuItem::new("save".to_string(), "Save");
+  let load_menu = tauri::CustomMenuItem::new("load".to_string(), "Load");
+  let file_submenu = tauri::Submenu::new("File", tauri::Menu::new()
+    .add_item(save_menu)
+    .add_item(load_menu));
   
   // define preference menu
   let device_menu = tauri::CustomMenuItem::new("device".to_string(), "Devices");
@@ -42,8 +51,7 @@ pub fn build_menu() -> tauri::Menu {
     .add_item(device_menu));
   
   tauri::Menu::new()
-    .add_native_item(tauri::MenuItem::Copy)
-    .add_item(tauri::CustomMenuItem::new("preferences", "Preferences"))
+    .add_submenu(main_submenu)
     .add_submenu(file_submenu)
     .add_submenu(preference_submenu)
 }
